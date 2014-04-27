2bcdefe1d0fd5c65c47e5630c29bc15ef4f17b49
elizabeth ocallaghan <eocallaghan@eocallaghan-Latitude-E6430.(none)>
2014-04-26 20:18:51 -0400

    model and model subclasses effectively complete

--- mechanicsGame.py
+++ mechanicsGame.py
@@ -11,6 +11,11 @@ import random
 import math
 import time
 
+"""  """  """  """  """  """  """  """  """  """  """  """  """  """  """
+                                Model
+"""  """  """  """  """  """  """  """  """  """  """  """  """  """  """
+"""--------------------------------Drawable----------------------------------"""
+
 class Drawable(object):
     __metaclass__ = ABCMeta
     @abstractmethod
@@ -19,6 +24,9 @@ class Drawable(object):
     @abstractmethod
     def translate(self,deltaPosition):
         pass
+    
+   
+"""--------------------------------Body and Planent----------------------------------"""
     
 class body(Drawable):
     def __init__(mass, radius, position, velocity, color):
@@ -48,26 +56,45 @@ class body(Drawable):
     def deltaPositon1D(self,accel,vel,time):
         return (vel*time + .5*accel*time**2);
         
+    def update(self, force, time):
+        deltaPos = self.deltaPosition(force, time);
+        self.translate(deltaPos);
+   
 class planet(body):
     def __init__(mass, radius, position, velocity, color):
         super(planet,self).__init__(mass, radius, position, velocity, color);
-    
-class thruster(Drawable):
-    def __init__(self, radius, position, color):
+        
+"""--------------------------------dependantBody, rocketBody, thruster----------------------------------"""
+        
+class dependantBody(Drawable):
+    def __init__(mass, radius, position, color):
+        self.mass = mass;
         self.radius = radius;
         self.position = position;
         self.color = color;
-        
-    def draw(self,screen):
+    
+    def draw(self, screen):
         pygame.draw.circle(screen, self.color, self.position, self.radius, 0);
         
     def translate(self, deltaPosition):
         self.position(0) += deltaPosition(0);
         self.position(1) += deltaPosition(1);
+     
+
+class rocketBody(dependantBody):
+    def __init__(mass, radius, position, color = (72,209,204)):
+        super(rocketBody,self).__init__( mass, radius, position, color);
+        
+        
+class thruster(dependantBody):
+    def __init__(self, radius, position, color = (255,54,54)):
+        super(rocketBody,self).__init__( mass, radius, position, color);
 
     def fire(self):
         return 123; #kJ of potential energy spent by emiting one fart of liquid hydrogen
-        
+
+"""--------------------------------thrusterSet----------------------------------"""
+   
 class thrusterSet():
     def __init__(mass,radius, positions):
         self.mass = mass;
@@ -77,22 +104,86 @@ class thrusterSet():
         
     def fireThruster(self,index):
         energyFired = self.thrusterList[index].fire();
-        direction = 1;
-        if(index == 2 or 3):
-            direction = -1;
+        direction = [0,0];
+        if (index == 0):
+            direction = [0 , 1];
+        elif(index == 1):
+            direction = [-1 , 0];
+        elif(index == 2 ):
+            direction = [0 , -1];
+        elif(index == 3):
+            direction = [1 , 0];
         return [direction, energyFired];
 
+    def translate(self, deltaPosition):
+        for thruster in self.thrusterList:
+            thruster.translate(deltaPosition);
 
-class rocketBody(body):
-    def __init__(mass, radius, position, velocity, color = (72,209,204)):
-        super(rocketBody, mass, radius, position, velocity, color);
+"""--------------------------------Rocket----------------------------------"""
         
 class rocket():
     def __init__(bMass, bRadius, bPosition, tmass, tradius, velocity):
         self.body = rocketBody(bMass, bRadius, bPosition, velocity);
         x = bPosition(0);
         y = bPosition(1);
+        self.acceleration = [0,0]
         self.thrusters = thrusterSet(tmass, tradius, (x,y-bRadius), (x+bRadius,y),(x,y+bRadius),(x-bRadius,y));
         
-       
+    def fireThruster(self, index):
+        poof = self.thruster.fireThruster(index);
+        energy = poof[1];
+        directinon = poof[0];
+        force = energy/self.body.radius;
+        directionalForce = [force*direction[0], force*direction[1]];
+        return directionalForce;
         
+    def calculateAcceleration(self, force):
+        mass = bMass + self.thrusters.mass;
+        self.acceleration = [force[0]/mass, force[1]/mass];
+        return self.acceleration;
+    
+    def deltaPosition(self,force, time):
+        self.calculateAcceleration(force);
+        deltaX = deltaPosition1D(self.acceleration(0),self.velocity(0), time);
+        deltaY = deltaPosition1D(self.acceleration(1),self.velocity(1), time);
+        return [deltaX, deltaY];
+        
+    def deltaPositon1D(self,accel,vel,time):
+        return (vel*time + .5*accel*time**2);
+    
+    def translate(self,deltaPosition):
+        self.body.translate(deltaPosition);
+        self.thrusters.translate(deltaPosition);
+        
+    def update(self, force, time):
+        deltaPos = self.deltaPosition(force, time);
+        self.translate(deltaPos);
+
+class model():
+    def __init__(rbMass, rbRadius, position, tmass, tradius):
+        self.planentsList = []
+        self.myRocket = rocket(rbMass, rbRadius, position, tmass, tradius, [0,0]);
+        self.generatePlanets(1);
+        
+    def generatePlanets(self,numPlanets):
+        pass
+    
+    def update(self, planetForces, rocketForce, time):
+        for i in range(len(planetForces)):
+            self.planetsList[i].update(planetForces[i], time);
+        self.rocket.update(rocketForce, time);
+        
+    def fireRocket(self, index):
+        self.myRocket.fireThruster(index);
+    
+    def objectsInWorld(self):
+        return [self.myRocket, self.planentsList];
+    
+            
+"""  """  """  """  """  """  """  """  """  """  """  """  """  """  """
+                                View
+"""  """  """  """  """  """  """  """  """  """  """  """  """  """  """
+
+"""  """  """  """  """  """  """  """  """  """  """  """  """  """  """
+                                Controller
+"""  """  """  """  """  """  """  """  """  """  """  """  """  """  """
